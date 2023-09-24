863ab47dab3d717d98ef6e5a813ebfc183df978c
dobrqka <dobrin.andreev@gmail.com>
2023-09-24 23:35:41 +0300

    Add back button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const allAnimalsButton = document.querySelector(".all-animals-button");
 const catsButton = document.querySelector(".cat-button");
 const dogsButton = document.querySelector(".dog-button");
 const hensButton = document.querySelector(".hen-button");
-const allPlantsButton = document.querySelector(".hen-button");
+const allPlantsButton = document.querySelector(".all-plants-button");
 const tomatoButton = document.querySelector(".tomato-button");
 const eggplantButton = document.querySelector(".eggplant-button");
 
@@ -24,6 +24,9 @@ const allCategoryButtons = mainCategoryButtons.concat(
 );
 
 const categoriesPage = document.querySelector(".category-buttons");
+const backButton = document.querySelector(".back-button");
+const backButtonTwo = document.querySelector(".back-button2");
+const galleryPage = document.querySelector(".galleries");
 
 // used when you click on a category
 const hideCategoryButtons = (categoryArray) => {
@@ -42,17 +45,23 @@ const showByCategory = (htmlClass) => {
 
 allCategoryButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    backButton.style.display = "block";
     const dataType = e.target.getAttribute("data-type");
     if (e.target.classList.contains("end")) {
       hideCategoryButtons(mainCategoryButtons);
       showByCategory(dataType);
+      backButtonTwo.style.display = "block";
+      galleryPage.className = "galleries";
+      galleryPage.classList.add(dataType);
     } else {
       if (dataType == ".animals") {
         hideCategoryButtons(mainCategoryButtons);
         showCategoryButtons(animalCategoryButtons);
+        galleryPage.classList.add("all-animals");
       } else if (dataType == ".plants") {
         hideCategoryButtons(mainCategoryButtons);
         showCategoryButtons(plantCategoryButtons);
+        galleryPage.classList.add("all-plants");
       }
     }
   });
@@ -74,9 +83,50 @@ const hideImages = () => {
   });
 };
 
-const backButton = document.querySelector(".back-button");
 // back to home
-backButton.addEventListener("click", () => {
-  showCategoryButtons(mainCategoryButtons);
-  hideImages();
+
+const classCheck = (className) => {
+  return galleryPage.classList.contains(className);
+};
+
+[backButton, backButtonTwo].forEach((button) => {
+  button.addEventListener("click", () => {
+    if (
+      // galleryPage.classList.contains(".all") ||
+      // galleryPage.classList.contains("all-animals") ||
+      // galleryPage.classList.contains("all-plants")
+      classCheck(".all") ||
+      classCheck("all-animals") ||
+      classCheck("all-plants")
+    ) {
+      hideCategoryButtons(allCategoryButtons);
+      showCategoryButtons(mainCategoryButtons);
+      hideImages();
+      backButton.style.display = "none";
+      backButtonTwo.style.display = "none";
+      galleryPage.className = "galleries";
+      console.log("hey");
+    } else if (
+      classCheck(".animals") ||
+      classCheck(".cats") ||
+      classCheck(".dogs") ||
+      classCheck(".hens")
+    ) {
+      hideImages();
+      showCategoryButtons(animalCategoryButtons);
+      galleryPage.className = "galleries";
+      galleryPage.classList.add("all-animals");
+      backButtonTwo.style.display = "none";
+    } else if (
+      classCheck(".plants") ||
+      classCheck(".tomatoes") ||
+      classCheck(".eggplants")
+    ) {
+      hideImages();
+      showCategoryButtons(plantCategoryButtons);
+      galleryPage.className = "galleries";
+      galleryPage.classList.add("all-plants");
+      backButtonTwo.style.display = "none";
+    }
+  });
 });
